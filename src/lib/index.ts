1e0d554aac9fb754708c5927d1e0636d59e4dc6f
Alastair <a.zotos@waracle.com>
2021-02-20 21:14:03 +0000

    exported new default theme and new extendDefaultTheme

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,7 @@
 export * from '@bitmetro/cms-common';
 export * from '@bitmetro/content-renderer'
 export * from './components';
+export { defaultTheme, extendDefaultTheme } from './defaultTheme';
+export * from './defaultTheme/items';
+
+
